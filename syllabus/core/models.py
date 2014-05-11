109e4761401fdd9157da5653014b8dff0a7fe42c
Alec Aivazis <alec@aivazis.com>
2014-05-10 21:47:46 -0700

    core : began adding models from syllabus-0.5

--- syllabus/core/models.py
+++ syllabus/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# syllabus core models
+# most of these are misc models used throughout the application
+
+# encapsulates generic metaDeta about an object
+class MetaData(models.Model):
+    key = models.CharField(max_length=1020)
+    value = models.CharField(max_length=1020)    
+    
+# points to a file in the system
+class File(models.Model):
+    fileName = models.CharField(max_length=4096)    
