e147c3971203fa1410e16660781d47935b7e7b3a
Alec Aivazis <alec@aivazis.com>
2014-05-10 22:53:04 -0700

    core : touched up comments

--- syllabus/core/models.py
+++ syllabus/core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # syllabus core models
 # most of these are misc models used throughout the application
 
-# encapsulates generic metaDeta about an object
+# encapsulates generic metaDeta about an object as a key value pair
 class MetaData(models.Model):
     key = models.CharField(max_length=1020)
     value = models.CharField(max_length=1020)    
