702802700c34a561696884a31d9f6faaa80fe6f8
Alec Aivazis <alec@aivazis.com>
2014-05-11 22:19:21 -0700

    Messages: replaced vviews.py with views folder

---
+++ syllabus/messages/views/announcementViews.py
@@ -0,0 +1,59 @@
+from common import *
+
+def newAnnouncement(request):
+    classes = Class.objects.filter(professor = request.user)
+    sections = Section.objects.filter(tas= request.user)
+    groups = UserGroup.objects.filter(managers = request.user)
+    
+    return render_to_response('/announcements/newAnnouncement.html', locals())
+   
+def create(request):
+    
+    if 'message' in request.POST and 'title' in request.POST and 'to' in request.POST:
+        
+        announcement = Announcement()
+        announcement.title=request.POST['title']
+        announcement.author = request.user
+        announcement.datePosted = datetime.datetime.now()
+        announcement.message = request.POST['message']
+        announcement.save()
+        
+        for target in request.POST['to'].split(','):
+            if target:
+                announcement.sections.add(Section.objects.get(id=target))
+        
+    
+    return HttpResponse('success!')
+    
+def home(request):
+    announcements = []
+    
+    if Class.objects.filter(professor=request.user) or UserGroups.objects.filter(managers = request.user):
+        canPost = True
+    
+    sections = []
+    for enrollment in Enrollment.objects.filter(student=request.user).filter(grade=''):
+        sections.append(enrollment.section)
+        for announcement in enrollment.section.announcements.all():
+            announcements.append(announcement)
+    
+    userGroups = UserGroup.objects.filter(users = request.user).order_by('name')
+    for group in userGroups.all():    
+        for announcement in group.announcements.all():
+            announcements.append(announcement)
+    
+    
+            
+    return render_to_response('/announcements/home.html', locals())
+
+def view(request):
+    if 'type' in request.GET and 'id' in request.GET:
+        if request.GET['type']=='section':
+            section = Section.objects.get(id=request.GET['id'])
+            announcements =  Announcement.objects.filter(sections = section).order_by('datePosted')
+            
+        elif request.GET['type'] == 'all':
+            announcements = Announcement.objects.all()
+            
+        return render_to_response('announcements/announcements.html', locals())
+            
