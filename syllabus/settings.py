ce0e07aa80f376dc1e14e89404fee4f3d56ae706
Alec Aivazis <alec@aivazis.com>
2014-05-12 01:30:17 -0700

    Syllabus: added various compressor/static file settings as well as user url permissions

--- syllabus/settings.py
+++ syllabus/settings.py
@@ -49,6 +49,13 @@ SYLLABUS = (
     'syllabus.socialservice',
 )
 
+# the url permissions of the various user groups (to be moved somewhere else)
+PERMS = {
+    'student': ['uploadify', 'gradebook', 'viewevent', 'calendar', 'calendarajax', 'admin','login', 'logout', 'viewclass', '', 'resources', 'myhomework','messageboard'],
+    'teacher' : ['','viewevent','uploadify',  'login', 'logout', 'viewclass', 'resources', 'calendar', 'calendarajax', 'gradebook', 'messageboard'],
+    'registrar' : ['','login', 'viewevent', 'uploadify', 'logout', 'viewclass', 'resources', 'registrar', 'messageboard']
+}
+
 # Application definition
 INSTALLED_APPS = DJANGO_APPS +  THIRD_PARTY + SYLLABUS
 
@@ -93,8 +100,42 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Resource settings
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'resources').replace('\\','/')
 
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = ''
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+STATIC_DOC_ROOT = os.path.join(os.path.dirname(__file__), 'resources/').replace('\\','/')
+STATIC_ADMIN_MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'Syllabus/media').replace('\\','/')
+
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static').replace('\\','/')
+COMPRESS_URL = '/static/'
 STATIC_URL = '/static/'
+
+COMPRESS_PRECOMPILERS =(
+    ('text/scss', 'sass {infile} > {outfile}'),
+)
+
+COMPRESS_ENABLED = False
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    # other finders..
+    'compressor.finders.CompressorFinder',
+)
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'resources/').replace('\\','/'),
+)
