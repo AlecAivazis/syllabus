e37e890251378bf483285858f378e37049f382fd
alec aivazis <alec@aivazis.com>
2015-04-21 18:03:07 -0700

    added academia application

---
+++ backend/syllabus/apps/academia/admin.py
@@ -0,0 +1,14 @@
+# -*- Python -*-
+# -*- coding: utf-8 -*-
+#
+# alec aivazis
+#
+# this file describes the administration for academia
+
+# import the django admin
+from django.contrib import admin
+
+# register the base models
+
+
+# end of file
