23f22c4c09c4d5c2e0c69fec5d50eaaf4e4b6791
alec aivazis <alec@aivazis.com>
2015-04-22 02:39:51 -0700

    moved navigation logic to NavBar component

--- web/src/app.js
+++ web/src/app.js
@@ -3,40 +3,15 @@
 'use strict';
 
 import NavBar from './navigation/navBar'
-import NavItem from './navigation/navItem'
-import _ from 'lodash'
+import 'babel/polyfill'
 
 // the base application component for syllabus
-
-// the current user's role (for now)
-let role = 'admin';
-
 class SyllabusRoot extends React.Component{
-
-    constructor(){
-        // create this
-        super();
-        // bind the various functions
-        this.getNavItems = this.getNavItems.bind(this);
-    }
-
-    // return the posible navigation items for the current user
-    getNavItems(){
-        return [
-            {
-                test: "hello"
-            }
-        ]
-    }
 
     // render the application
     render() {
         return (
-            <NavBar>
-                { _.map( this.getNavItems(), function(item) {
-                    return <NavItem message={item.test}/>
-                })}
-            </NavBar>
+            <NavBar />
         )
     }
 }
--- web/src/navigation/navBar.js
+++ web/src/navigation/navBar.js
@@ -1,15 +1,77 @@
 // the navigation bar used by syllabus
 
+// imports
+import NavItem from './navItem'
+import _ from 'lodash'
+
 'use strict'
 
+// the current user's role (for now)
+let roles = ['admin'];
+
+// the navigation bar component
 class NavBar extends React.Component {
+
+    constructor(){
+        // create this
+        super();
+        // bind the various functions
+        this.get_nav_routes = this.get_nav_routes.bind(this);
+    }
+
+    // return the posible navigation items for the current user
+    get_nav_routes(){
+
+        // define the possible routes 
+        let all_routes = new Map([
+            ['registrar', [
+                {
+                    name: 'hello',
+                    test: 'hello',
+                }
+
+            ]],
+            ['teacher', [
+                {
+                    name: 'hello',
+                    test: 'hello',
+                }
+
+            ]],
+            ['student', [
+                {
+                    name: 'hello',
+                    test: 'hello',
+                }
+            
+            ]]
+        ]);
+
+        for (var [key, value] of all_routes.entries()){
+            console.log(key);
+        }
+
+        // the routes the user can visit
+        let routes = [];
+        // for each role of the user        
+        _.each(roles, role => {
+            // add the routes for this role to the list
+            routes = _.union(routes, all_routes.get(role));
+        });
+
+        // return the list of routes
+        return routes;
+    }
+
 
     render() {
         // get the current user's role
 
         return (
             <div>
-                {this.props.children}
+                { _.map( this.get_nav_routes(), function(item) {
+                    return <NavItem message={item.test} key={item.name}/>
+                })}
             </div>
         )
     }
