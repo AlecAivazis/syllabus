fa3367e241348187c147a33ad87595d6a4a16fbd
alec aivazis <alec@aivazis.com>
2015-04-24 01:57:52 -0700

    replaced gulp with webpack for building

---
+++ web/webpack.config.js
@@ -0,0 +1,33 @@
+// frontend build configuration using webpack
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: './assets', // This is where images AND js will go
+        publicPath: "./src/", //path that wil be considered when requiring your filesl
+        filename: 'app.js'
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.js$/,
+                loader: "eslint-loader?{rules:[{semi:0}]}",
+                exclude: /node_modules/,
+            },
+        ],
+        loaders: [
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }, // inline base64 URLs for <=8k images, direct URLs for the rest
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+            { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader' }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.styl']
+    },
+    eslint: {
+        configFile: '.eslintrc'
+    }
+};
+
+// end of file
