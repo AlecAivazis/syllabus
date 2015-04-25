68e1aa47e0358de24310190b21137bec142d6a22
alec aivazis <alec@aivazis.com>
2015-04-24 20:08:11 -0700

    replaced metadata field with local model

--- backend/syllabus/apps/academia/models/university.py
+++ backend/syllabus/apps/academia/models/university.py
@@ -1,11 +1,12 @@
 # django imports
 from django.db import models
 
-class University(models.Model):
+from syllabus.apps.metadata.models import MetadataModel
+
+class University(MetadataModel):
     """
     Responsible for university wide parameters.
     """
     name = models.CharField(max_length=1020)
-    min_grade_to_pass = models.IntegerField(default=50)
 
 # end of file
