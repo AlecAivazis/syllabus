68c351515e8118abb36b1bbceb7c754e0362728c
alec aivazis <alec@aivazis.com>
2015-04-24 21:13:44 -0700

    added minimum grade to pass as a GradeField

--- backend/syllabus/apps/academia/models/university.py
+++ backend/syllabus/apps/academia/models/university.py
@@ -2,6 +2,7 @@
 from django.db import models
 # local imports
 from syllabus.apps.metadata.decorators import metadata_model
+from syllabus.fields import GradeField
 
 @metadata_model
 class University(models.Model):
@@ -9,6 +10,7 @@ class University(models.Model):
     Responsible for university wide parameters.
     """
     name = models.CharField(max_length=1020)
+    min_grade_to_pass = GradeField()
 
 
 # end of file
