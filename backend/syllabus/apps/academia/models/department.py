c1ab9d1849e7037f5f882cb80fe50bc5bd87649f
alec aivazis <alec@aivazis.com>
2015-04-24 21:23:37 -0700

    added foreign key to college

--- backend/syllabus/apps/academia/models/department.py
+++ backend/syllabus/apps/academia/models/department.py
@@ -6,9 +6,10 @@ class Department(models.Model):
     a `department` of a `college` with some `interests`
     """
     name = models.CharField(max_length=1020)
-    interests = models.ManyToManyField('Interest', related_name="department")
+    college = models.ForeignKey('College', related_named="departments")
+    # majors = models.ManyToManyField('Major', related_name="college", blank=True)
 
-    def __unicode__(self):
+    def __str__(self):
         """ string behavior is to return the name """
         return self.name 
 
