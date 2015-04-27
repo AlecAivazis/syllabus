36d136eda97ff4bf7a6c5633718417da21a9490e
alec aivazis <alec@aivazis.com>
2015-04-27 00:53:18 -0700

    added empty tests for tabPanel

--- web/src/components/tabs/tabPanel/tests/test_component.js
+++ web/src/components/tabs/tabPanel/tests/test_component.js
@@ -1,11 +1,54 @@
+// test various functionalities of the TabPanel component 
+// uses mocha and chai
+
+// react
+import React from 'react';
+// facebook test utils
+import TestUtils from 'react/lib/ReactTestUtils';
+// local imports
 import TabPanel from '../component';
+import Tab from '../../tab/component';
 
-describe("Tab Panel", function() {
+describe("Tab Panel", () => {
 
-    beforeEach(function() { });
-    afterEach(function() { });
+    // define the components to be rendered to the dom
+    let tab1 = <Tab title="tab1">tab1</Tab>;
+    let tab2 = <Tab title="tab2">tab2</Tab>
+    let panel_component = (
+        <TabPanel>
+            {tab1}
+            {tab2}
+        </TabPanel>
+    )
 
-    it( 'should fail', function() { 
-        expect(true).to.be.true; 
+
+    it('renders to the DOM', function() {
+        // render the component to the DOM
+        let rendered_component = TestUtils.renderIntoDocument(panel_component);
+        // check that it worked
+        expect(rendered_component).to.exist;
+    });
+
+
+    it('fails if no tabs are given', function(){
+
+    });
+
+
+    it('shows the first tab if no default set', function(){
+
+    });
+
+
+    it('allows for a default tab to be set', function(){
+
+    });
+
+
+    it('can switch between views', function() {
+
     });
 });
+
+
+// end of file
