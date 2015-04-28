cd24bad46770742768f7bcadd7db6d0deda35906
alec aivazis <alec@aivazis.com>
2015-04-28 03:48:08 -0700

    added white background to tabs

---
+++ web/src/components/tabs/tab/styles.js
@@ -0,0 +1,12 @@
+// the styles used by the tab component
+
+// local imports
+import colors from 'styles/colors';
+
+
+export let tab_style = {
+    background: colors.white,
+};
+
+
+// end of file
