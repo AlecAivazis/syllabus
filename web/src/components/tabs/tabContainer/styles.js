43d742faf7006ac682eacf61bbbd327a0869713a
alec aivazis <alec@aivazis.com>
2015-04-27 17:09:07 -0700

    added missing files

---
+++ web/src/components/tabs/tabContainer/styles.js
@@ -0,0 +1,21 @@
+// the styles used by the TabContainer component
+
+export let menuElementStyle = {
+    display: 'inline-block',
+};
+
+export let listContainerStyle = {
+    float: 'left',
+    padding: '0',
+    margin: '0'
+};
+
+export let menuToolbarStyle = {
+    float: 'right',
+};
+
+export let headerStyle = {
+    padding: '10px',
+};
+
+// end of file
