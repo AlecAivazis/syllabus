9cc49f4b9d955e17bf9f23c4e9866f056c259dee
alec aivazis <alec@aivazis.com>
2015-04-28 03:31:56 -0700

    added a better white to the color pallete

--- web/styles/colors.js
+++ web/styles/colors.js
@@ -1,6 +1,7 @@
 // this module exports the color pallete used throughout the application
 export default {
-    backgroundColor: '#dfddd1'
+    backgroundColor: '#dfddd1',
+    white: '#f9f8f0'
 }
 
 // end of file
