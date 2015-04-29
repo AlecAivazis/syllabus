42d732092d522b14384a3c8cd46c95da6e0efdbf
alec aivazis <alec@aivazis.com>
2015-04-28 22:34:55 -0700

    tabs are now clearfixed

--- web/src/components/tabs/tab/component.js
+++ web/src/components/tabs/tab/component.js
@@ -4,6 +4,7 @@
 import React from 'react';
 // local imports
 import {tab_style} from './styles';
+require('styles/clearfix.styl');
 
 'use strict'
 
@@ -13,9 +14,9 @@ class Tab extends React.Component {
     render() {
         // return the image
         return (
-            <article style={tab_style}>
+            <div style={tab_style} className="clearfix">
                 {this.props.children}
-            </article> 
+            </div> 
         )
     }
 }
