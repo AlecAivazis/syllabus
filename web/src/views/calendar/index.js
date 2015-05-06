f09d5b99d072a814247baf5a30b84fc7b4fab1a3
alec aivazis <alec@aivazis.com>
2015-05-06 01:39:20 -0700

    fixed capitalization error

--- web/src/views/calendar/index.js
+++ web/src/views/calendar/index.js
@@ -3,7 +3,7 @@
 // react: https://github.com/facebook/react
 import React from 'react'
 // local imports
-import TabContainer from 'components/tabs/TabContainer/component'
+import TabContainer from 'components/tabs/tabContainer/component'
 import Tab from 'components/tabs/tab/component'
 import MonthCalendar from 'components/calendar/monthCalendar/component'
 import WeeklyCalendar from 'components/calendar/weeklyCalendar/component'
