7c94ec97c4e6d6981c3b2ca8ec6f8dbf7417be6b
alec aivazis <alec@aivazis.com>
2015-05-07 03:54:04 -0700

    added view for rendering 'isomorphic' components

--- backend/syllabus/views.py
+++ backend/syllabus/views.py
@@ -7,12 +7,29 @@
 
 # django imports
 from django.views.generic import TemplateView
+# python-react: https://github.com/markfinger/python-react
+from react.render import render_component
 
 class Home(TemplateView):
     """
     render the index template
     """
-    template_name = 'index.html'
+    template_name = 'index.jade'
+
+
+class ComponentView(TemplateView):
+    """
+    render the index template
+    """
+    template_name = 'component.jade'
+
+    def get_context_data(self, **kwargs):
+        # grab the parent context
+        context = super().get_context_data()
+        # add the rendered component to the context
+        context['component'] = render_component(self.component_path, translate=True)
+        # return the modified context
+        return context
 
 
 # end of file
